93f2a612b1769ea0a16ab8c7125ec22aa493d8fc
unknown <rone.caballero@gmail.com>
2019-02-12 20:25:07 -0800

    Fixed bug where edmx file won't display in solutions explorer.

--- Products/ProductService/Program.cs
+++ Products/ProductService/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,13 @@ namespace ProductService
     {
         static void Main(string[] args)
         {
+            using (ServiceHost host = new ServiceHost(typeof(WCFProductService)))
+            {
+                host.Open();
+                Console.WriteLine("Server is open!");
+                Console.WriteLine("<Press Enter to close server>");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ Products/ProductsInterfaces/IWCFProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProductInterfaces
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWCFProductService" in both code and config file together.
+    [ServiceContract]
+    public interface IWCFProductService
+    {
+        [OperationContract]
+        List<string> ListProducts();
+    }
+}
